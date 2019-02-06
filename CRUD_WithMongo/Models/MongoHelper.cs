ab0e6d86b8c8a8d7f6f7a2c6572d817a3b2f57c5
areaintel <hbastow@gmail.com>
2019-02-06 14:52:49 -0800

    initial commit

---
+++ CRUD_WithMongo/Models/MongoHelper.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using System.Configuration;
+
+namespace CRUD_WithMongo.Models
+{
+    public class MongoHelper
+    {
+        public static IMongoClient client { get; set; }
+        public static IMongoDatabase database { get; set; }
+        public static string MongoDatabase = "crud_mongodb";
+        public static string MongoConnection = ConfigurationManager.ConnectionStrings["MongoConnection"].ConnectionString;
+        // You might need to replace & with &amp; in your Mongo Connection string stored in Web.Config
+        internal static void ConnectToMongoService()
+        {
+            try
+            {
+                client = new MongoClient(MongoConnection);
+                database = client.GetDatabase(MongoDatabase);
+                // If you look in your https://cloud.mongodb.com  database overview you 
+                // should see the connection and graph change, you may need to whitelist your IP
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
